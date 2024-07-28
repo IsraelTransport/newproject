90ffc356baa296128ea90d7b831423f138426517
IsraelTransport <chatgptuser885@gmail.com>
2024-07-28 20:33:16 +0300

    newbcryptjs

--- src/Users/Users.Controller.js
+++ src/Users/Users.Controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { getUserByUsername, getUsersFromDB, createUserInDB, getLastUserID } = require('./Users.db');
 
 const userTypeMap = {
