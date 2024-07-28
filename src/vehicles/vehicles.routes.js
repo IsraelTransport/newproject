9222d962cbe0d79f09075f9214363df35b38d51a
IsraelTransport <chatgptuser885@gmail.com>
2024-07-28 18:59:03 +0300

    Update vehicles.routes.js

--- src/vehicles/vehicles.routes.js
+++ src/vehicles/vehicles.routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const { getVehicles, getVehicle, createVehicle, updateVehicle, deleteVehicle } = require('./Vehicles.Controller');
+const { getVehicles, createVehicle, updateVehicle, deleteVehicle } = require('./vehicles.controller');
 const router = express.Router();
 
 router.get('/GetAllVehicles', getVehicles);
-router.get('/GetVehicle/:id', getVehicle);
 router.post('/CreateVehicle', createVehicle);
 router.put('/UpdateVehicle/:id', updateVehicle);
 router.delete('/DeleteVehicle/:id', deleteVehicle);
