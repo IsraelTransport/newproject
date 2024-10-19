da506220d632defec451d33d20c300000822d1e7
IsraelTransport <chatgptuser885@gmail.com>
2024-10-19 12:44:19 +0300

    newfix

--- Counter/counters.db.js
+++ Counter/counters.db.js
@@ -11,15 +11,14 @@ async function getNextSequenceValue(collectionName) {
         await mongo.connect();
         const database = mongo.db(DB_INFO.name);
         const collection = database.collection(collectionName);
-        
-        const lastDocument = await collection.find().sort({ userID: -1 }).limit(1).toArray();
-        
-        if (lastDocument.length === 0) {
-            return 1;  
-        } else {
-            return lastDocument[0].userID + 1;  
-        }
+
+        // Count the number of documents in the collection
+        const count = await collection.countDocuments();
+
+        // Return the next ID as count + 1
+        return count + 1;
     } catch (error) {
+        console.error('Error getting next sequence value:', error);
         throw error;
     } finally {
         await mongo.close();
